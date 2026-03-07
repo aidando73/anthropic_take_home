50c9c0d918d93bb02318a58e82858a909358b697
Aidan Do <aidando73@gmail.com>
2026-03-07 15:37:32 -0800

    .

--- debug.py
+++ debug.py
@@ -1,8 +1,19 @@
-from frozen_problem import (
+from problem import (
     Tree,
-    Input
+    Input,
+    reference_kernel
 )
+
+forest_height=10
+rounds=16
+batch_size=256
 
 forest = Tree.generate(forest_height)
 inp = Input.generate(forest, batch_size, rounds)
 
+# print(forest)
+# print(inp)
+
+print(reference_kernel(forest, inp))
+
+print(inp)
